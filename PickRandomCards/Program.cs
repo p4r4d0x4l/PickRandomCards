2a4ab22effeee5d60b457b71f7dff236c18540fa
p4r4d0x4l <263694@usn.no>
2026-01-28 15:57:34 +0100

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nEnter the numbers of cards you want to pick: ");
+
+            string line = Console.ReadLine();
+
+            Console.WriteLine();    // tom linje for penere utskrift
+
+            if (int.TryParse(line, out int numberOfCards)) 
+            {
+                // this block is executed if line COULD be converted to an int
+                // value stored in variable numberOfCards
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards)) 
+                {
+                    Console.WriteLine(card);
+                }
+                Console.WriteLine();    // tom linje
+            }
+            else 
+            {
+                // this block is executed if line COULD NOT be converted to an int
+                Console.WriteLine("\nPlease enter a valid card number.\n\n");
+            }
+        }
+    }
+}
